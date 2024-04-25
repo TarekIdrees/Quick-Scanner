e79a8c15f8978541985acd438eebb2429e0a801e
TarekIdrees <tarekidrees5@gmail.com>
2024-04-26 00:19:02 +0200

    setup scanner dependencies injection

---
+++ app/src/main/java/com/tareq/barcodescanner/di/ScannerModule.kt
@@ -0,0 +1,37 @@
+package com.tareq.barcodescanner.di
+
+import android.app.Application
+import android.content.Context
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object ScannerModule {
+    @Provides
+    @ViewModelScoped
+    fun provideContext(application: Application): Context {
+        return application.applicationContext
+    }
+
+    @Provides
+    @ViewModelScoped
+    fun provideOptions(): GmsBarcodeScannerOptions {
+        return GmsBarcodeScannerOptions
+            .Builder()
+            .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+            .build()
+    }
+    @Provides
+    @ViewModelScoped
+    fun provideScanner(context: Context, options: GmsBarcodeScannerOptions): GmsBarcodeScanner {
+        return GmsBarcodeScanning.getClient(context, options)
+    }
+}

5db8d4649a08856069b6459fbe974ee1f685612f
TarekIdrees <tarekidrees5@gmail.com>
2024-04-30 16:54:49 +0200

    add preview annotation class

---
+++ core/design-system/src/main/java/com/tareq/design_system/components/BarcodeScannerPreview.kt
@@ -0,0 +1,19 @@
+package com.tareq.design_system.components
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(
+    name = "Preview Day",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    backgroundColor = 0
+)
+@Preview(
+    name = "Preview Night",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    backgroundColor = 0
+)
+annotation class BarcodeScannerPreview

c5619e0e4a556e1f5a9e1f544772fefe5134ec86
TarekIdrees <tarekidrees5@gmail.com>
2024-05-05 13:13:43 +0200

    remove enable edge to edge

--- app/src/main/java/com/tareq/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/tareq/barcodescanner/MainActivity.kt
@@ -2,16 +2,12 @@ package com.tareq.barcodescanner
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tareq.design_system.ui.BarcodeScannerTheme
 import com.tareq.design_system.ui.background
@@ -23,10 +19,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb()),
-            navigationBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb())
-        )
         setContent {
             BarcodeScannerTheme {
                 Scaffold(

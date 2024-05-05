b601eb58df877c74994081e44889c0ec653e3c3f
TarekIdrees <tarekidrees5@gmail.com>
2024-05-05 13:09:42 +0200

    change system and navigation bar color

--- core/design-system/src/main/java/com/tareq/design_system/ui/Theme.kt
+++ core/design-system/src/main/java/com/tareq/design_system/ui/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -40,6 +41,8 @@ fun BarcodeScannerTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            window.statusBarColor = LightColorScheme.background.toArgb()
+            window.navigationBarColor = LightColorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }

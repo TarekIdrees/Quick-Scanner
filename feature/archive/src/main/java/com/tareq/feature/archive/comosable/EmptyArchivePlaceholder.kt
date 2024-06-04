594800de7bb82d511193b66807162249ab484a6e
TarekIdrees <tarekidrees5@gmail.com>
2024-06-04 13:45:00 +0200

    change placeholder items spacing

--- feature/archive/src/main/java/com/tareq/feature/archive/comosable/EmptyArchivePlaceholder.kt
+++ feature/archive/src/main/java/com/tareq/feature/archive/comosable/EmptyArchivePlaceholder.kt
@@ -5,13 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,14 +27,13 @@ internal fun EmptyArchivePlaceholder(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.Center,
     ) {
         Image(
+            modifier = Modifier.fillMaxWidth(),
             painter = painterResource(id = R.drawable.image_empty_archive_placeholder),
             contentDescription = "empty archive placeholder",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp)
+            contentScale = ContentScale.Crop,
         )
         Text(
             text = stringResource(R.string.empty_archive),
--- feature/scanner/src/main/java/com/tareq/scanner/composable/ErrorPlacehoder.kt
+++ feature/scanner/src/main/java/com/tareq/scanner/composable/ErrorPlacehoder.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -19,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -53,11 +53,10 @@ internal fun ErrorPlaceholder(
             )
         }
         Image(
+            modifier = Modifier.fillMaxWidth(),
             painter = painterResource(id = R.drawable.error_placholder),
             contentDescription = "connection error placeholder",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp)
+            contentScale = ContentScale.Crop
         )
         Text(
             text = stringResource(messageFile),
